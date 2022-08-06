5cbc9eb0494a96b64db4659da66de69d3886ccef
crobibero <cody@robibe.ro>
2022-08-06 19:44:29 -0400

    import prebuilts from SolidHal/android-auto-stub, add LOCAL_PRODUCT_MODULE

---
+++ speechservicestub/Android.mk
@@ -0,0 +1,21 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE := speechservicestub
+LOCAL_SRC_FILES := speech-services-stub.apk
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_PRODUCT_MODULE := true
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE := speechservicestub-unsigned
+LOCAL_SRC_FILES := speech-services-unsigned-stub.apk
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_CERTIFICATE := release
+LOCAL_PRODUCT_MODULE := true
+include $(BUILD_PREBUILT)
