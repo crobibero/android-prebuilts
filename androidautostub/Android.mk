8d7c628038a528ae4fa7def7d823a1931f39b421
crobibero <cody@robibe.ro>
2022-08-06 20:36:17 -0400

    TARGET_OUT_ETC -> TARGET_OUT_PRODUCT_ETC

--- androidautostub/Android.mk
+++ androidautostub/Android.mk
@@ -2,23 +2,20 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := privapp-permissions-com.google.android.projection.gearhead.xml
-LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/permissions
+LOCAL_MODULE_PATH := $(TARGET_OUT_PRODUCT_ETC)/permissions
 LOCAL_SRC_FILES := privapp-permissions-com.google.android.projection.gearhead.xml
 LOCAL_REQUIRED_MODULES := default-permissions.xml
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := default-permissions.xml
-LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/default-permissions
+LOCAL_MODULE_PATH := $(TARGET_OUT_PRODUCT_ETC)/default-permissions
 LOCAL_SRC_FILES := default-permissions.xml
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := AndroidAuto
 LOCAL_SRC_FILES := AndroidAutoStubPrebuilt.apk
 LOCAL_MODULE_CLASS := APPS
--- auroraservices/Android.mk
+++ auroraservices/Android.mk
@@ -2,14 +2,12 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := privapp-permissions-com.aurora.services.xml
-LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/permissions
+LOCAL_MODULE_PATH := $(TARGET_OUT_PRODUCT_ETC)/permissions
 LOCAL_SRC_FILES := $(LOCAL_MODULE)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := auroraservices
 LOCAL_SRC_FILES := AuroraServices_v1.1.1.apk
 LOCAL_MODULE_CLASS := APPS
